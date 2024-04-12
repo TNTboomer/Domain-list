c3500b3cc3b9881fb1074c43c1bafca123a04750
TNTboomer <60656884+TNTboomer@users.noreply.github.com>
2024-04-12 13:40:55 +0800

    Update meituxiuxiuCrack.js

--- meituxiuxiuCrack.js
+++ meituxiuxiuCrack.js
@@ -1,3 +1,4 @@
-hostname = *.xiuxiu.meitu.com
-# ✅ 美图秀秀
+[rewrite_local]
 ^https?://(api|h5).xiuxiu.meitu.com/(?!(v1/feed/)) url script-response-body https://github.com/ddgksf2013/dev/raw/main/MeiTuXiuXiuProCrack.js
+[mitm]
+hostname = *.xiuxiu.meitu.com
