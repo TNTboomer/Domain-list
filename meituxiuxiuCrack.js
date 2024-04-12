490dae94ec5725ca3291f9435d2eb63a033f5d43
TNTboomer <60656884+TNTboomer@users.noreply.github.com>
2024-04-12 13:32:14 +0800

    Create meituxiuxiuCrack.js

---
+++ meituxiuxiuCrack.js
@@ -0,0 +1,3 @@
+hostname = *.xiuxiu.meitu.com
+# ✅ 美图秀秀
+^https?://(api|h5).xiuxiu.meitu.com/(?!(v1/feed/)) url script-response-body https://github.com/ddgksf2013/dev/raw/main/MeiTuXiuXiuProCrack.js
